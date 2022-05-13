1e4a7a041d2f1e03d961839848e8eb4c8809aa59
franncesco <francesco.coriolano@gmail.com>
2022-05-13 09:20:36 +0200

    created a random number generator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
 import Die from "./Components/Die";
 function App() {
+  const allNewDice = () => {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      let die = Math.floor(Math.random() * 6) + 1;
+      newDice.push(die);
+    }
+    return newDice;
+  };
+
+  console.log(allNewDice());
+
   return (
     <main className="main">
       <h1 className="title">Tenzies</h1>
