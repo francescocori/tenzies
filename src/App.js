1f8073c5ec99fc7e973eca429ee69efca161ea3b
franncesco <francesco.coriolano@gmail.com>
2022-05-13 08:55:59 +0200

    created basic style  strucutre

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="die-container">
+        <div className="die">1</div>
+        <div className="die">2</div>
+        <div className="die">3</div>
+        <div className="die">4</div>
+        <div className="die">5</div>
+        <div className="die">6</div>
+        <div className="die">7</div>
+        <div className="die">8</div>
+        <div className="die">9</div>
+        <div className="die">10</div>
+      </div>
+    </main>
   );
 }
 
